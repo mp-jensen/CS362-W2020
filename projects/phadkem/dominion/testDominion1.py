802f79189e6b9d5bb7afd9546be7a8c9e99e7099
phadkem <51807611+phadkem@users.noreply.github.com>
2020-01-13 15:45:20 -0600

    add test scenario of changing players after game setup to testDominion1

--- projects/phadkem/dominion/testDominion1.py
+++ projects/phadkem/dominion/testDominion1.py
@@ -9,13 +9,18 @@ import testUtility
 
 
 #Set the player names
-player_names = ["Annie","*Ben","*Carla"]
+player_names = ["Annie"]
 
 #get the card box
 box = testUtility.getBox(player_names)
 
-#get the card supply for this specific game
+#get the card supply for this instance of the game
 supply = testUtility.getSupply(box, player_names)
+
+#Introduce Test Scenario: change the number of players after 
+#the supply has already been set up
+player_names.append("*Richard")
+player_names.append("*Julie")
 
 #get the player objects
 players = testUtility.getPlayers(player_names)
